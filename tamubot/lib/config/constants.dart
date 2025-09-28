43da9f27779b3767bab3e7b17d422a486eb678d6
Kinuthia Claudia <claudiawkinuthia@gmail.com>
2025-09-28 23:42:39 +0300

    Deletion

--- tamubot/lib/config/constants.dart
+++
@@ -1,4 +0,0 @@
-class Constants {
-  static const String supabaseUrl = 'https://gnkvcfoatmbpuoonyxeu.supabase.co';
-  static const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdua3ZjZm9hdG1icHVvb255eGV1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg2MjczNDEsImV4cCI6MjA3NDIwMzM0MX0.Xs0mkt81nVnDwdINCE9bna9h-IURaU9eecH0tfCh3aE';
-}
